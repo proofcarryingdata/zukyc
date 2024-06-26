7da133ad1fb3c1d1154a4846cdb96dea7c0e6213
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-06-26 01:55:45 -0700

    fix deployment

--- apps/issuer-server/src/index.ts
+++ apps/issuer-server/src/index.ts
@@ -1,14 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
-const app: Express = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+const main = async () => {
+  const app: Express = express();
+  app.use(cors());
+  app.use(express.json());
+  const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
+
+  app.listen(port, () => {
+    console.log(`[server]: Server ready on port ${port}`);
+  });
+};
+
+main();
