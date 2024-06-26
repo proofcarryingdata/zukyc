cb768bcd3d62ee5049cd6261c8582adfd3cdf24e
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-06-26 14:28:52 -0700

    issue pod

--- apps/issuer-server/src/index.ts
+++ apps/issuer-server/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { POD, PODEntries } from "@pcd/pod";
 
 dotenv.config();
+
+const EDDSA_PRIVATE_KEY = process.env.EDDSA_PRIVATE_KEY || "";
 
 const main = async () => {
   const app: Express = express();
@@ -11,7 +14,34 @@ const main = async () => {
   const port = process.env.PORT || 3001;
 
   app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    res.send("Zukyc Server");
+  });
+
+  app.post("/issue", (req: Request, res: Response) => {
+    const inputs: {
+      firstName: string;
+      lastName: string;
+      age: bigint;
+      semaphoreCommitment: bigint;
+    } = req.body;
+
+    // Verify the inputs here
+
+    // If verified, issue pods
+    // For more info, see https://github.com/proofcarryingdata/zupass/blob/main/examples/pod-gpc-example/src/podExample.ts
+    const podEntries: PODEntries = {
+      firstName: { type: "string", value: inputs.firstName },
+      lastName: { type: "string", value: inputs.lastName },
+      age: { type: "int", value: inputs.age },
+      semaphoreId: {
+        type: "cryptographic",
+        value: inputs.semaphoreCommitment
+      }
+    };
+    const pod = POD.sign(podEntries, EDDSA_PRIVATE_KEY);
+    console.log(JSON.stringify(pod.content.asEntries));
+    const serializedPOD = pod.serialize();
+    res.status(200).json({ pod: serializedPOD });
   });
 
   app.listen(port, () => {
