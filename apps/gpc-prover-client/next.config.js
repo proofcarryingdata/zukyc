a8563a8abf5a27bbb814d7fdf8c581485a7951c6
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-06-26 23:58:36 -0700

    fix Can't import the named export 'O_CREAT' from fastfile

--- apps/gpc-prover-client/next.config.js
+++ apps/gpc-prover-client/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false
+      };
+    }
+
+    return config;
+  }
 };
