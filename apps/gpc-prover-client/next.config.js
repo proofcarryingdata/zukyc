5334c0aa6daf2908745b8cef449bb982f1d60ef5
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-06-27 17:04:35 -0700

    fix polyfill for fastfile

--- apps/gpc-prover-client/next.config.js
+++ apps/gpc-prover-client/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   transpilePackages: ["@repo/ui"],
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         fs: false
+      };
+      config.resolve.alias = {
+        constants: require.resolve(
+          "rollup-plugin-node-polyfills/polyfills/constants"
+        ),
+        process: "process/browser"
       };
     }
 
