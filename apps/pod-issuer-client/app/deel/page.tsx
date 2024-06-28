a4f57056b2bf9bbd263ddb699d08445b443397f0
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-06-28 14:25:28 -0700

    update readme

--- apps/pod-issuer-client/app/deel/page.tsx
+++ apps/pod-issuer-client/app/deel/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Deel() {
   return (
-    <main className="p-0 m-0 flex flex-col gap-2 h-screen max-h-screen">
-      <div className="flex gap-4 flex-1 p-2">Paystub POD issuer</div>
+    <main className="p-6 m-0 flex flex-col gap-4 h-screen max-h-screen">
+      <h1 className="text-xl font-bold">ZooDeel</h1>
+      <h2 className="text-lg">Paystub POD issuer</h2>
+      <p>Under construction</p>
     </main>
   );
 }
