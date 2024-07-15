7222ead4a41c26fa415ceb3ff27dd755159ae714
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-07-15 00:17:24 -0700

    implement deel login

--- apps/pod-issuer-server/dist/routes/deel.js
+++ apps/pod-issuer-server/dist/routes/deel.js
@@ -4,11 +4,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_jwt_1 = require("express-jwt");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const pod_1 = require("@pcd/pod");
 const constants_1 = require("../util/constants");
 const deel = express_1.default.Router();
-// TODO: implement login, authenticate
-deel.post("/issue", (req, res) => {
+deel.post("/login", (req, res) => {
+    const inputs = req.body;
+    if (!inputs.email || !inputs.password) {
+        res.status(400).send("Missing query parameter");
+        return;
+    }
+    // In practice, get user information from database by email,
+    // Here for demo purposes, we'll allow any email with @zoo.com domain
+    if (!inputs.email.endsWith("@zoo.com")) {
+        res.status(401).send("Invalid email or password");
+        return;
+    }
+    // In practice, check if the encrypted password match
+    // This is just for demo purposes
+    if (!inputs.password.startsWith("zoo")) {
+        res.status(401).send("Invalid email or password");
+        return;
+    }
+    // Signing JWT, valid for 1 hour
+    const token = jsonwebtoken_1.default.sign({ email: inputs.email }, process.env.DEEL_EDDSA_PRIVATE_KEY, { algorithm: "HS512", expiresIn: "1h" });
+    res.send(token);
+});
+deel.post("/issue", (0, express_jwt_1.expressjwt)({
+    secret: process.env.DEEL_EDDSA_PRIVATE_KEY,
+    algorithms: ["HS512"]
+}), (req, res) => {
     const inputs = req.body;
     if (!inputs.semaphoreCommitment) {
         res.status(400).send("Missing query parameter");
