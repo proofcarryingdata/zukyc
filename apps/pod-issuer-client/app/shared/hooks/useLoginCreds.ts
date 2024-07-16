d4bb6f252ff693d23632ce9fdb97f5104a310e28
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2024-07-15 21:20:54 -0700

    remove ui package

--- apps/pod-issuer-client/app/shared/hooks/useLoginCreds.ts
+++ apps/pod-issuer-client/app/shared/hooks/useLoginCreds.ts
@@ -3,6 +3,9 @@ import Chance from "chance";
 import useStore from "@/shared/hooks/useStore";
 
 // Generate random login credentials for demo purposes
+// We reuse the same email for "gov" and "deel" websites.
+// On the server side, they generate the same firstName and lastName for the same email.
+// Our proof will check we have the same firstName and lastName for ID POD and paystub POD.
 // In practice, need to implement sign up process
 const useLoginCreds = () => {
   const email = useStore((state) => state.email);
